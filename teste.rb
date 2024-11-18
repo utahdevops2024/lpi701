d831bf57ca15445dd40e9240bb169cd9130d469e
Fabio Pires <fpires@utah.com.br>
2024-11-18 12:14:25 -0300

    Alterado o arquivo teste.rbt

--- teste.rb
+++ teste.rb
@@ -1,7 +1,3 @@
 Teste
-<<<<<<< HEAD
 console.log("Esta Mudanca esta sendo feita na branch master em um outro ponto da historia")
-=======
-
 console.log("Agora estamos atuando na Branch testing")
->>>>>>> testing
